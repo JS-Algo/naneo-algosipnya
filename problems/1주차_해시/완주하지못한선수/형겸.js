b58271a4cc70169e5f664b6167e60ef13cd76b86
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-18 17:57:17 +0900

    feat : 완주하지못한선수

--- problems/1주차_해시/완주하지못한선수/형겸.js
+++ problems/1주차_해시/완주하지못한선수/형겸.js
@@ -1,21 +1,21 @@
 function solution(participant, completion) {
   let answer = "";
 
-  let pH = new Map();
+  let hashMap = new Map();
 
   for (let p of participant) {
-    pH.set(p, pH.get(p) + 1 || 1);
+    hashMap.set(p, hashMap.get(p) + 1 || 1);
   }
 
   for (let c of completion) {
-    if (pH.get(c) === 1) {
-      pH.delete(c);
+    if (hashMap.get(c) === 1) {
+      hashMap.delete(c);
     } else {
-      pH.set(c, pH.get(c) - 1);
+      hashMap.set(c, hashMap.get(c) - 1);
     }
   }
 
-  for (let [key, value] of pH) {
+  for (let [key, value] of hashMap) {
     answer = key;
   }
 
