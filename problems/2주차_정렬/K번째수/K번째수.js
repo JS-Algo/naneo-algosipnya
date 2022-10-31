5cc913ba9dfc26dcd99a8367efd655f4f79b71dc
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-31 19:51:55 +0900

    feat : K번째 수(진행중)

---
+++ problems/2주차_정렬/K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+  let answer = [];
+
+  for (let command of commands) {
+    let sliceArray = array.slice(command[0] - 1, command[1]);
+    sliceArray.sort();
+    answer.push(sliceArray[command[2] - 1]);
+  }
+  return answer;
+}
+
+// 테스트 2번 아직 통과 못함,, 계속 시도중
