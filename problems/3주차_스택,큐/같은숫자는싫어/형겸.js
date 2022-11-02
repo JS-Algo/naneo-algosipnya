d5e9c66716f3cf83d0e990aad861df831aa759cc
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-02 21:38:57 +0900

    chore : 설명 주석 및 추가 풀이

--- problems/3주차_스택,큐/같은숫자는싫어/형겸.js
+++ problems/3주차_스택,큐/같은숫자는싫어/형겸.js
@@ -3,7 +3,8 @@ function solution(arr) {
 
   for (let i = 0; i < arr.length; i++) {
     if (answer[answer.length - 1] !== arr[i]) {
-      answer.push(arr[i]);
+      // 현재 순회중인 수 가 마지막에 넣은 수와 과 같지 않다면
+      answer.push(arr[i]); // answer 배열로 push
     }
   }
 
