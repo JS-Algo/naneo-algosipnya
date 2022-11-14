6a72e9ea5f9ca47d59264681af187c638bee53e6
Eunhyung Choi <milkiss0414@naver.com>
2022-11-14 13:36:33 +0900

    Update: 올바른 괄호

--- problems/3주차_스택큐/올바른괄호/은형.js
+++ problems/3주차_스택큐/올바른괄호/은형.js
@@ -1,33 +1,33 @@
 // 좀 더 빠른 풀이
-function solution(s){
-    let stack = [];
-    
-    Array.from(s).map((c) => {
-        let prev = stack[stack.length - 1];
-        stack.push(c);
-        
-        if(prev === "(" && c === ")") {
-            stack.pop();
-            stack.pop();
-        }
-    })
+function solution(s) {
+  let stack = [];
 
-    return stack.length > 0 ? false : true;
+  Array.from(s).map((c) => {
+    let prev = stack[stack.length - 1];
+    stack.push(c);
+
+    if (prev === "(" && c === ")") {
+      stack.pop();
+      stack.pop();
+    }
+  });
+
+  return stack.length > 0 ? false : true;
 }
 
-function solution(s){
-    let stack = [];
-    
-    for(c of Array.from(s)) {
-        if(c === "(") {
-            stack.push("(")
-        } else if (c === ")" && stack.length > 0) {
-            stack.pop()
-        } else {
-            // stack이 비어있는 상태에서 ")"를 만나면
-            return false;
-        }
+function solution(s) {
+  let stack = [];
+
+  for (c of Array.from(s)) {
+    if (c === "(") {
+      stack.push("(");
+    } else if (c === ")" && stack.length > 0) {
+      stack.pop();
+    } else {
+      // stack이 비어있는 상태에서 ")"를 만나면
+      return false;
     }
-    
-    return stack.length > 0 ? false : true;
+  }
+
+  return stack.length > 0 ? false : true;
 }
