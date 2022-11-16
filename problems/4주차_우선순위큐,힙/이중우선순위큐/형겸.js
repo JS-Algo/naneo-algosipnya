f3c0567f5409e634a464c6fd76756179e10b0819
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-16 20:36:37 +0900

    feat : 이중우선순위큐

--- problems/4주차_우선순위큐,힙/이중우선순위큐/형겸.js
+++ problems/4주차_우선순위큐,힙/이중우선순위큐/형겸.js
@@ -1,13 +1,15 @@
 function solution(operations) {
   const heap = [];
-  const command = operations.map((operation) => operation.split(" "));
+  const commands = operations.map((operation) => operation.split(" ")); // operation 배열의 요소를 빈칸을 기준으로 나눔
 
-  command.forEach((num) => {
-    if (num[0] === "I") {
-      heap.push(Number(num[1]));
+  commands.forEach((command) => {
+    if (command[0] === "I") {
+      // 문자열의 인덱스 0번이 "I"  이면
+      heap.push(Number(command[1])); // 숫자로 변환하여 heap 에 push
     } else {
+      // 그 외의 경우(문자열의 인덱스 0 번이 "D" 이면)
       heap.splice(
-        heap.indexOf((num[1] === "1" ? Math.max : Math.min)(...heap)),
+        heap.indexOf((command[1] === "1" ? Math.max : Math.min)(...heap)), // 인덱스 1번이 1이면 최댓값, 그게 아니라면(-1 이라면) 최솟값 을 찾아 삭제
         1
       );
     }
