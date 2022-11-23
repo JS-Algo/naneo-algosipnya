5aca12e298c3489794c96e177acf41be4dc2b2fc
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-23 19:42:36 +0900

    chore : 체육복 주석추가

--- problems/5주차_그리디/체육복/형겸.js
+++ problems/5주차_그리디/체육복/형겸.js
@@ -1,15 +1,16 @@
 function solution(n, lost, reserve) {
-  const allLost = lost
-    .sort((a, b) => a - b)
+  const allLost = lost // 체육복을 도난당하고, 여벌도 없는 학생 수
+    .sort((a, b) => a - b) // 오름차순 정렬
     .filter((lost) => !reserve.includes(lost));
-  const noLostReserve = reserve
+  const noLostReserve = reserve // 도난당하지 않고 여벌도 있는 학생 수
     .sort((a, b) => a - b)
     .filter((reserve) => !lost.includes(reserve));
 
   const losted = allLost.filter((lost) => {
+    // 최종적으로 체육수업을 나갈 수 없는 학생 구하기
     const lend = noLostReserve.find((reserve) => Math.abs(reserve - lost) == 1);
 
-    if (!lend) return lost;
+    if (!lend) return lost; // 아무도 빌려줄 수 없는 상황이면 그대로 lost 반환
     noLostReserve = noLostReserve.filter((reserve) => reserve !== lend);
   });
 
