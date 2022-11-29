db4bac8c4938c94aef7783932902ec7cbf07405a
seoyeun0106 <sarah.mun16@gmail.com>
2022-11-29 20:11:03 +0900

    Feat: 최소직사각형

---
+++ problems/6주차_완전탐색/최소직사각형/서연.js
@@ -0,0 +1,23 @@
+// function solution(sizes) {
+//   const smallNums = [];
+//   const concattedArr = sizes.reduce((acc, cur) => {
+//     const [num, diffNum] = cur;
+//     smallNums.push(num > diffNum ? diffNum : num);
+//     return [...acc, ...cur];
+//   }, []);
+//   concattedArr.sort((a, b) => {return a - b;});
+//   smallNums.sort((a, b) => {return a - b});
+//   const biggestNum = concattedArr[concattedArr.length - 1];
+//   const biggerNum = smallNums[smallNums.length - 1];
+//   return biggerNum * biggestNum;
+// }
+
+function solution(sizes) {
+  let horizontal = 1;
+  let vertical = 1;
+  sizes.forEach((el) => {
+    if (horizontal < Math.max(...el)) horizontal = Math.max(...el);
+    if (vertical < Math.min(...el)) vertical = Math.min(...el);
+  });
+  return horizontal * vertical;
+}
