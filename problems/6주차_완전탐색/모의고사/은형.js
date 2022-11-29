240fb31aebdb8cd572d011f2275d7749188a6923
Eunhyung Choi <milkiss0414@naver.com>
2022-11-29 17:02:59 +0900

    Update: 모의고사 reduce함수 쓴 풀이

--- problems/6주차_완전탐색/모의고사/은형.js
+++ problems/6주차_완전탐색/모의고사/은형.js
@@ -20,3 +20,26 @@ function solution(answers) {
 
   return answer;
 }
+
+// reduce 함수 써보기
+function solution(answers) {
+  let solves = {
+    1: [1, 2, 3, 4, 5],
+    2: [2, 1, 2, 3, 2, 4, 2, 5],
+    3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  };
+  let bingos = [0, 0, 0]; // 맞춘문제 수
+
+  for (let i = 0; i < answers.length; i++) {
+    if (solves[1][i % solves[1].length] === answers[i]) bingos[0] += 1;
+    if (solves[2][i % solves[2].length] === answers[i]) bingos[1] += 1;
+    if (solves[3][i % solves[3].length] === answers[i]) bingos[2] += 1;
+  }
+
+  let maxBingo = Math.max(...bingos);
+
+  return bingos.reduce((acc, cur, idx) => {
+    if (cur >= maxBingo) acc.push(idx + 1);
+    return acc;
+  }, []);
+}
