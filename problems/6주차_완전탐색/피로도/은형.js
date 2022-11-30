b0e99b4668385299a18556280972c45610f9af22
Eunhyung Choi <milkiss0414@naver.com>
2022-11-30 16:07:10 +0900

    Feat: 피로도

---
+++ problems/6주차_완전탐색/피로도/은형.js
@@ -0,0 +1,34 @@
+const getPermutations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((value) => [value]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((permutation) => [fixed, ...permutation]);
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+function solution(k, dungeons) {
+  var answer = -1;
+
+  getPermutations(dungeons, dungeons.length).forEach((orders) => {
+    let fatigue = k,
+      count = 0;
+
+    for ([minimum, consume] of orders) {
+      if (fatigue < 0) break;
+      if (fatigue >= minimum) {
+        fatigue -= consume;
+        count++;
+      }
+    }
+
+    answer = count > answer ? count : answer;
+  });
+
+  return answer;
+}
